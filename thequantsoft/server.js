6ebaad099221360dc80a5532711d8207ae945324
raushaniit2020 <raushaniit.2020@gmail.com>
2026-08-18 20:39:13 +0530

    Refactor code structure for improved readability and maintainability

--- thequantsoft/server.js
+++ thequantsoft/server.js
@@ -20,6 +20,10 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'landing.html')));
 app.get('/blog', (req, res) => res.sendFile(path.join(__dirname, 'blog.html')));
 app.get('/blog-post', (req, res) => res.sendFile(path.join(__dirname, 'blog-post.html')));
+app.get(/^\/blog\/([A-Za-z0-9-]+)\/?$/, (req, res) => {
+  const slug = String(req.params[0]).replace(/[^a-z0-9-]/gi, '');
+  res.sendFile(path.join(__dirname, 'blog-' + slug + '.html'), (err) => { if (err) res.redirect('/blog'); });
+});
 
 // Expose configuration securely to frontend
 app.get('/config.js', (req, res) => {
